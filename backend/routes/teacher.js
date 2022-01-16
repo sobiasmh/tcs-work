1e5f5213cde56e53b1a2852e4a4f6a8687034adb
sobiasmh <sobiaa930@gmail.com>
2022-01-16 13:29:53 +0500

    new

--- backend/routes/teacher.js
+++ backend/routes/teacher.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose')
 var Class = require('../models/class');
-var Quiz = require('../models/quiz');
 
 var Admin = require('../models/admin');
 var Student = require('../models/student');
@@ -13,19 +12,19 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/addquiz', function(req, res, next) {
-  Quiz.create(req.body)
-      .then((quiz) => {
-          console.log('Quiz has been Added ', quiz);
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json(result);
-      }, (err) => next(err))
-      .catch((err) => next(err));
-});
+router.post('/addQuiz', function(req, res, next) {
+    Class.create(req.body)
+        .then((result) => {
+            console.log('Quiz has been Added ', result);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(result);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+  });
 
-router.get('/quiz', function(req, res, next) {
-    Quiz.find().sort('question').exec(function(error, results) {
+  router.get('/quiz', function(req, res, next) {
+    Class.find("quizes").sort('quizes').exec(function(error, results) {
         if (error) {
             return next(error);
         }
@@ -35,7 +34,7 @@ router.get('/quiz', function(req, res, next) {
   });
 
   router.get('/quiz/:id', function(req, res, next) {
-    Quiz.findById(req.params.id)
+    Class.findById(req.params.id)
         .then((quiz) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
@@ -46,20 +45,5 @@ router.get('/quiz', function(req, res, next) {
   });
 
 
-router.put('/assign/:cid/Quiz/:qid', function(req, res, next) {
-    Class.findOneAndUpdate({ _id: req.params.cid }, {
-            "$push": {
-                "quizes": {
-                    "qid": req.params.qid
-                }
-            }
-        }, { new: true, upsert: false },
-        function(error, results) {
-            if (error) {
-                return next(error);
-            }
-            // Respond with valid data
-            res.json(results);
-        });
-  });
+
 module.exports = router;
